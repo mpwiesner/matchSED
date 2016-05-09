91f9b0293892fee669f31b4cf288b8adbf512a63
mpwiesner <matthewwiesner@aol.com>
2016-05-09 12:44:44 -0400

    Made separate redshifted and restframe programs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 This package uses the lsst/sims_photUtils package in order to match a set of galaxy magnitudes to an SED. 
 
-First, you should install the DMStack.  I recommend using Anaconda.  If you have Anaconda, follow instructions here:
+First, you should install the DMStack.  I recommend using Anaconda.  If you don't have Anaconda, download and install it.  
+If you have Anaconda, follow instructions here to install DMStack:
 
 https://gist.github.com/mjuric/1e097f2781bc503954c6
 
@@ -9,11 +10,16 @@ Next, initialize the lsst_sims package using these commands:
 source eups-setups.sh
 setup lsst_sims 
 
-Next run my code to call the selectGalaxySED package, using this command:
+If your input magnitudes are in the rest frame, run this code to call the selectGalaxySED package:
 
-python matcher.py
+python matcher_restframe.py
 
-You will then get a list of SEDs chosen by selectGalaxySED written to the file 'SEDmatch.out'
+If your input magnitudes are as observed at a particular redshift, run this code to call the selectGalaxySED package:
+
+python matcher_redshift.py
+
+
+Either program will write a list of SEDs chosen by selectGalaxySED to the file 'SEDmatch.out'
 
 Then open IDL and run SEDplot.pro using this code:
 
---
+++ matcher_redshift.py
@@ -0,0 +1,47 @@
+from lsst.sims.photUtils.selectGalaxySED import selectGalaxySED
+import os
+import numpy as np
+import lsst.utils
+from lsst.sims.photUtils.BandpassDict import BandpassDict
+
+#If you are using your own folder this should be selectGalaxySED(galDir = yourFolder)
+matchSED = selectGalaxySED()
+"""Can restrict the loading below to a subset of the SEDs in a given folder if you pass a list
+as matchSED.loadBC03(subset = subsetList)"""
+sedList = matchSED.loadBC03()
+
+print sedList[1].wavelen
+print sedList[1].flambda
+print sedList[1].name
+
+#Import sample galaxy catalog organized as [RA, Dec, z, mag_g, mag_r, mag_i, mag_z, mag_y]
+ra, dec, z, mag_g, mag_r, mag_i, mag_z, mag_y = np.genfromtxt('sampleGalCat.dat', unpack=True)
+
+"""Organize magnitudes into single array with each row corresponding to the 
+set of magnitudes of one object"""
+catMagsObs = np.transpose([mag_g, mag_r, mag_i, mag_z, mag_y])
+
+#Define bandpasses. This example shows loading in LSST grizy bandpasses.
+galBandpassDict = BandpassDict.loadTotalBandpassesFromFiles(['g', 'r', 'i', 'z', 'y'])
+
+#We'll need to set makeCopy to True since we are going to reuse this sedList below
+#matchObservedNames, matchObsMagNorm, matchObsErrors = matchSED.matchToObserved(sedList, catMagsObs, z, catRA=ra, catDec=dec, 
+#                                                                               bandpassDict = galBandpassDict, dzAcc=2,extCoeffs = (3.0999999,3.0999999,3.0999999,3.0999999,3.0999999,3.0999999))  #(4.145, 3.237, 2.273, 1.684, 1.323, 1.088))
+
+matchObservedNames, matchObsMagNorm, matchObsErrors = matchSED.matchToObserved(sedList, catMagsObs, z, catRA=ra, catDec=dec, dzAcc = 2,
+																			   bandpassDict = galBandpassDict,
+																			   extCoeffs = (3.237, 2.273, 1.684, 1.323, 1.088))	 
+                                                                              
+#Show the resulting matches
+#print matchObservedNames
+#print matchObsMagNorm
+#print matchObsErrors
+
+print matchObservedNames
+print matchObsMagNorm
+print matchObsErrors
+
+phile = open("matchSED.out","w")
+
+for h in range (0, len(matchObservedNames)):
+	print >>phile, matchObservedNames[h][:-3]
